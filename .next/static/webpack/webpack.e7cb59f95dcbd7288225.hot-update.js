696398580d7a8fb14945fb1593eb3f0e73e0c1df
Matt Long <60803733+MattLong34@users.noreply.github.com>
2021-06-15 08:39:24 -0600

    first commit

---
+++ .next/static/webpack/webpack.e7cb59f95dcbd7288225.hot-update.js
@@ -0,0 +1,12 @@
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "2f95fa46db0332da5deb"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FL3dlYnBhY2svcnVudGltZS9nZXRGdWxsSGFzaCJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7VUFBQSxvQ0FBb0MsK0JBQStCLEUiLCJmaWxlIjoic3RhdGljL3dlYnBhY2svd2VicGFjay5lN2NiNTlmOTVkY2JkNzI4ODIyNS5ob3QtdXBkYXRlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gZnVuY3Rpb24oKSB7IHJldHVybiBcIjJmOTVmYTQ2ZGIwMzMyZGE1ZGViXCI7IH0iXSwic291cmNlUm9vdCI6IiJ9
